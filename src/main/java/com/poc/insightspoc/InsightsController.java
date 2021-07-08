e67bb9844d14ed64cb97814eb697e2c73e360af3
raghudevendra <31237949+raghudevendra@users.noreply.github.com>
2021-07-08 12:50:43 +0530

    Add files via upload

---
+++ src/main/java/com/poc/insightspoc/InsightsController.java
@@ -0,0 +1,16 @@
+package com.poc.insightspoc;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class InsightsController {
+
+
+
+    @GetMapping("/welcome")
+    public String hello()
+    {
+        return "Hello azure portal";
+    }
+}
---
+++ src/test/java/com/poc/insightspoc/InsightspocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poc.insightspoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsightspocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
